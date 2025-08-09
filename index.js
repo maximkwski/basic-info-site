2b73069e60ececfb512891a30cdc907e91fd2a92
Maksym <makkie.makowski@gmail.com>
2025-08-09 14:34:54 -0400

    refactor to express

--- index.js
+++ index.js
@@ -1,57 +1,30 @@
-const http = require('http')
+const express = require('express')
 const path = require('path')
-const fs = require('fs')
+require('dotenv').config()
 
-const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
-
-    let extname = path.extname(filePath)
-
-    // Initial content type
-  let contentType = "text/html";
-
-  // Check ext and set content type
-  switch (extname) {
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-      contentType = "image/jpg";
-      break;
-  }
-
-  // Check if contentType is text/html but no .html file extension
-  if (contentType == "text/html" && extname == "") filePath += ".html";
-
-  //read file
-  fs.readFile(filePath , (err, content) => {
-    if (err) {
-        if(err.code == 'ENOENT') {
-            fs.readFile(path.join(__dirname, 'public', '404.html'),
-        (err, content) => {
-            res.writeHead(200, {'content-type' : 'text/html'})
-            res.end(content, 'utf-8')
-        })
-        } else {
-            res.writeHead(500)
-            res.end(`Server Error: ${err.code}`)
-        }
-    } else {
-        res.writeHead(200, {'content-type' : contentType})
-        res.end(content, 'utf-8')
-    }
-  })
-})
-
+const app = express()
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'))
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact-me.html'))
+})
+
+
+app.use('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+  console.log(`Visit: http://localhost:${PORT}`)
+})
